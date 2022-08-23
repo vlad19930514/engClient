06716b463dedfa46650f77c4d218ab0c4c47b53c
Vladislav Moskalenko <62288939+vlad19930514@users.noreply.github.com>
2022-08-23 14:15:58 +0300

    build fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,11 @@
 import Layout from '@/components/Layout/Layout'
-import { useTypedSelector } from '@/hooks/useTypedSelector'
 import { TypeComponentAuthFields } from '@/shared/types/auth.types'
-import { setTheme } from '@/store/settings/settings.slice'
 import { store } from '@/store/store'
 import type { AppProps } from 'next/app'
 import AuthProvider from 'providers/AuthProvider/AuthProvider'
 import ThemeProviderLocal from 'providers/ThemeProvider/ThemeProvider'
-import { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
-import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '../styles/GlobalStyle'
-import { darkTheme, defaultTheme } from '../styles/theme'
 
 type TypeAppProps = AppProps & TypeComponentAuthFields
 
